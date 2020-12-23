0d144ad2039651f29f2fbacf7a8a1193f00b9dac
bondappetit <bondappetit@https://users.noreply.github.com/>
2020-12-23 08:39:30 +0000

    new build 1.6.2-hardhat on 2020-12-23 08:39:30

--- ropsten/gen/assets.js
+++ ropsten/gen/assets.js
@@ -1,13 +1,13 @@
 module.exports = {
     "Bond": {
-        "address": "0x990081EBF8d5E2C3618a487998a159a2D8393FE3",
+        "address": "0xd966cA4ccd672F8A6A1ac2EFC60998db05D7CCf1",
         "name": "Bond",
         "symbol": "BOND",
         "decimals": 18,
         "investing": false
     },
     "ABT": {
-        "address": "0x62fe44C98eC3221bF13674435F4fCD597f64ADED",
+        "address": "0x7aD53995F0b26F3C9575635E01E0e86Fa7065e81",
         "name": "ABT",
         "symbol": "ABT",
         "decimals": 18,
